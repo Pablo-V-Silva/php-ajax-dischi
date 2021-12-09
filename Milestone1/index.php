e08a4f3842a93d9fa06eae1066efa6d560f7ecc8
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-12-09 15:34:15 +0100

    base structur and nav bar

---
+++ Milestone1/index.php
@@ -0,0 +1,17 @@
+<?php
+  require __DIR__ . '/particles/head.php';
+?>
+<body>
+  <header>
+    <?php
+    require __DIR__ . '/particles/nav.php'
+    ?>
+  </header>
+
+  <main>
+    <?php 
+    require __DIR__ . '/particles/albums.php'
+    ?>
+  </main>
+</body>
+</html>
---
+++ Milestone1/particles/albums.php
@@ -0,0 +1,10 @@
+<div class="container">
+  <div class="card">
+    <img src="" alt="">
+
+    <div class="text-light">
+      <h4>lorem</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae rem ut illo eaque nulla reprehenderit doloribus laudantium modi, consectetur tenetur!</p>
+    </div>
+  </div>
+</div>
