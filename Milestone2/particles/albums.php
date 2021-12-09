c05fb70581fb6c14d4c26b5fef8ed662572beb5e
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-12-09 15:58:03 +0100

    Milestone 2 structure

---
+++ Milestone2/particles/albums.php
@@ -0,0 +1,15 @@
+<div class="container py-5">
+  <div class="row g-5">  
+    <div class="col-3">
+      <div class="card">
+        <img src="" alt="">
+
+        <div class="text-light">
+          <h4>lorem</h4>
+          <p>ipsum</p>
+          <p>iner</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ Milestone2/particles/nav.php
@@ -0,0 +1,3 @@
+<nav class="py-4" >
+   <img src="./assets/img/logo.svg" alt="" height="100px">
+</nav>
